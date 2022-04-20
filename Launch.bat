2d02a3b1b220f05b8ad9c1e67c913a531645072b
Ken <98230827+ItzmeKen@users.noreply.github.com>
2022-04-20 11:00:40 +0530

    Create Launch.bat

---
+++ Launch.bat
@@ -0,0 +1,14 @@
+@echo off
+title Rattle Nuker
+
+if exist node_modules\ (
+  echo You've already installed this
+  echo Navigate to "config" folder for bot settings and "src" to start the script
+  pause
+  exit
+) else (
+  call npm i >> NUL
+  echo Succesfully installed!
+  echo Please re-run this file.
+  pause
+  exit
